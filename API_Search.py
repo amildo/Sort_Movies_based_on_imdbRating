4a65c1da05574859be5fb825603cbcbb331e43d2
amir mohammad Ghiasi <72264818+amildo@users.noreply.github.com>
2023-08-08 20:02:00 +0330

    Update API_Search.py

--- API_Search.py
+++ API_Search.py
@@ -6,7 +6,7 @@ def get_movie_data(movie_id):
 
     :param movie_id : you must enter the movie id. you can find it in IMDB website.
     :return: Information such as Title,ImdbRating,Year, ...
-    NOTE: if you want to use omdbapi you must sign in their website and get your free apikey.
+    NOTE: if you want to use omdbapi you must sign up in their website and get your free apikey.
     """
     param_d = {"i": movie_id, "r": "json", "apikey": "Enter Your Key Here"}
     url = "https://www.omdbapi.com/"
