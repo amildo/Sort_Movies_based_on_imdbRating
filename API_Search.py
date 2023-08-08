433329a5530187fd88d0ceafc2aef539cdd8cccf
amir mohammad Ghiasi <72264818+amildo@users.noreply.github.com>
2023-08-08 19:38:50 +0330

    Add files via upload

---
+++ API_Search.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def get_movie_data(movie_id):
+    """
+
+    :param movie_id : you must enter the movie id. you can find it in IMDB website.
+    :return: Information such as Title,ImdbRating,Year, ...
+    NOTE: if you want to use omdbapi you must sign in their website and get your free apikey.
+    """
+    param_d = {"i": movie_id, "r": "json", "apikey": "Enter Your Key Here"}
+    url = "https://www.omdbapi.com/"
+    data = requests.get(url=url, params=param_d)
+    print(data.url)
+    return data.json()
